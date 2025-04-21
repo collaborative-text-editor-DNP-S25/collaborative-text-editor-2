b54d9657c8ef87bb8bf21b8912836d24245c81f2
Ilia Kliantsevich <i.kliantsevich@innopolis.university>
2025-04-21 16:53:35 +0300

    feat: add delete document usecase

--- server/domain/DocumentRepositories.ts
+++ server/domain/DocumentRepositories.ts
@@ -2,5 +2,6 @@ export interface DocumentRepository {
     createDocument(): Promise<string>;
     getDocument(docId: string): Promise<Document | null>;
     updateDocument(docId: string, document: Document): Promise<void>;
+    deleteDocument(docId: string): Promise<string>;
     // TODO: declare methods for version history and deletion
 }
