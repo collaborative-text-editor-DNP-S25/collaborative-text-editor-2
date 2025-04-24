9dce09e43a649d0ba5979111806000502543566e
can4red <CAN4red@yandex.ru>
2025-04-24 13:04:02 +0300

    feat: implement disconnect to doc use case

---
+++ src/lib/server/domain/usecases/DisconnectFromDocumentUseCase.ts
@@ -0,0 +1,11 @@
+import { type DocumentId } from "$lib/server/domain/entities/Document";
+import type { SocketClient } from "$lib/server/domain/entities/SocketClient";
+import { type SocketRepository } from "$lib/server/domain/repositories/SocketRepository";
+
+export default class DisconnectFromDocumenUseCase {
+  constructor(private socketRepo: SocketRepository) {}
+
+  async invoke(client: SocketClient, docId: DocumentId): Promise<void> {
+    await this.socketRepo.unregisterClient(client, docId);
+  }
+}
