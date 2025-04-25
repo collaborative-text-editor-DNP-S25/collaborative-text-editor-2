c7be11fdeed7ed9d32a0066232be90cde9fb2024
Ilia Kliantsevich <i.kliantsevich@innopolis.university>
2025-04-25 13:14:08 +0300

    chore: removed TODOs which were already done

--- src/lib/server/domain/usecases/CreateDocumentUseCase.ts
+++ src/lib/server/domain/usecases/CreateDocumentUseCase.ts
@@ -3,7 +3,6 @@ import { type DocumentId } from "$lib/server/domain/entities/Document";
 
 export default class CreateToDocumenUseCase {
   constructor(private documentRepo: DocumentRepository) {}
-  // TODO: implement use case
   async invoke(): Promise<DocumentId> {
     const id = await this.documentRepo.createDocument();
     return id;
--- src/lib/server/domain/usecases/DeleteDocumentUseCase.ts
+++ src/lib/server/domain/usecases/DeleteDocumentUseCase.ts
@@ -3,7 +3,6 @@ import { type DocumentId } from "$lib/server/domain/entities/Document";
 
 export default class DeleteDocumenUseCase {
   constructor(private documentRepo: DocumentRepository) {}
-  // TODO: implement use case
   async invoke(docId: DocumentId): Promise<DocumentId> {
     const id = await this.documentRepo.deleteDocument(docId);
     return id;
